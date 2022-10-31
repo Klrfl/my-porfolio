6c24f4e16aa3c39920e44a2fdc3edd4ad4460a97
Efraim Munthe <efraimmunthe101@gmail.com>
2022-10-31 15:43:31 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>FORM VALIDATION!!</title>
+</head>
+<body>
+  <p>Welcome! I was practicing my PHP.</p>
+  <p>Your name is:</p>
+
+  <?php echo $_GET["name"]; ?>
+  <p>Is this really your name?</p>
+</body>
+</html>
