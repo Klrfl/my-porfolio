a6bf94d5d7ace6935a53359b58490a0a66df87d8
Efraim Munthe <efraimmunthe101@gmail.com>
2022-11-15 11:36:57 +0700

    added slider

--- js/script.js
+++ js/script.js
@@ -17,3 +17,26 @@ for(let i = 0; i < sidebarLinks.length; i++){
     sidebar.classList.remove("active");
   })
 }
+
+//slides init
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+});
