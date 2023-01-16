1bec2e3f396fb778f65069c8554fde07c0633f92
Efraim Munthe <efraimmunthe101@gmail.com>
2023-01-16 19:44:47 +0700

    formatting

--- js/script.js
+++ js/script.js
@@ -5,41 +5,41 @@ let sidebarOpen = document.getElementById("sidebar-open");
 let sidebarClose = document.getElementById("sidebar-close");
 let sidebarLinks = document.querySelectorAll(".navlinks");
 
-sidebarOpen.addEventListener('click', ()=>{
-  sidebar.classList.toggle("active");
-  body.classList.toggle("active");
-})
-
-sidebarClose.addEventListener('click', ()=>{
+sidebarOpen.addEventListener("click", () => {
   sidebar.classList.toggle("active");
   body.classList.toggle("active");
 });
 
-for(let i = 0; i < sidebarLinks.length; i++){
-  sidebar.addEventListener('click', ()=>{
+sidebarClose.addEventListener("click", () => {
+  sidebar.classList.toggle("active");
+  body.classList.toggle("active");
+});
+
+for (let i = 0; i < sidebarLinks.length; i++) {
+  sidebar.addEventListener("click", () => {
     sidebar.classList.remove("active");
-  })
+  });
 }
 
 //slides init
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper(".swiper", {
   // Optional parameters
-  direction: 'horizontal',
+  direction: "horizontal",
   loop: true,
 
   // If we need pagination
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 
   // Navigation arrows
   navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
 
   // And if we need scrollbar
   scrollbar: {
-    el: '.swiper-scrollbar',
+    el: ".swiper-scrollbar",
   },
 });
